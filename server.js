ebabba677b279795c63e72a58862a4094f025cce
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-09-09 19:11:36 -0400

    Full auth functionality

--- server.js
+++ server.js
@@ -2,25 +2,50 @@
 var express = require('express');
 var session = require('express-session');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-auth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var app = express();
 
 app.use(session({ secret: 'SOMETHINGSUPERSECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', function(req, res, next) {
+passport.serializeUser(function(user, done) {
+  done(null, user);
+
 });
 
-app.get('/authenticate', function(req, res, next) {
-  res.send('<p>HELLLO</p>');       
+passport.deserializeUser(function(user, done) {
+  done(null, user);
 });
 
-app.get('/authenticate/callback');
+passport.use(new GoogleStrategy({
+  clientID: '548770298027-6imrmo0s4sfatogovec2i9bsvr6ld1b4.apps.googleusercontent.com',
+  clientSecret: 'ADHfVW8-nyylcMNjO96xcPDk',
+  callbackURL: 'http://127.0.0.1:8060/authenticate/callback'
+}, function(accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+app.get('/', function(req, res) {
+  res.send('<a href="/authenticate">Log in here!</a>');
+});
+
+app.get('/authenticate', passport.authenticate('google', {
+  scope: 'https://www.googleapis.com/auth/userinfo.profile'
+}));
+
+app.get('/authenticate/callback', passport.authenticate('google', {
+  successRedirect: '/display',
+  failureRedirect: '/'
+}));
 
 app.get('/display', function(req, res) {
   if (req.user) {
-    res.send('You are ' + req.user.display + ' , and I claim my $1 million');
+    res.send('You are ' + req.user.displayName + ' , and I claim my $1 million');
   } else {
     res.redirect('/authenticate');
   }
 });
+
+app.listen(8060);
